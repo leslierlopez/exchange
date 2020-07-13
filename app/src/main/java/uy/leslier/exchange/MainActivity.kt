d1c6c97c81a90c80b5dd6db0557fbfe70b5d84d5
Leslier Lopez <leslier.lopez@despegar.com>
2020-07-13 20:51:33 -0300

    initial app

---
+++ app/src/main/java/uy/leslier/exchange/MainActivity.kt
@@ -0,0 +1,11 @@
+package uy.leslier.exchange
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
