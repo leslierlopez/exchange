866f17298e7fd2c2264393d484653726236aca52
Leslier Lopez <leslier.lopez@despegar.com>
2020-07-18 09:06:11 -0300

    Add Timber

---
+++ app/src/main/java/uy/leslier/exchange/App.kt
@@ -0,0 +1,13 @@
+package uy.leslier.exchange
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
+
